2d75b1771706b8f864ff525c10a375ae4d0e5cdb
Francisco Palm <francisco.palm@gmail.com>
2018-06-13 19:22:35 -0400

    Adding Notes to Admin

--- membership/admin.py
+++ membership/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 
 from .models import Note, MembershipTerm, Organization, Person, Membership
 
+class NoteInline(GenericTabularInline):
+    model = Note
+    max_num = 1
+
+class MembershipTermAdmin(admin.ModelAdmin):
+    inlines = [
+        NoteInline,
+    ]
+
+class OrganizationAdmin(admin.ModelAdmin):
+    inlines = [
+        NoteInline,
+    ]
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [
+        NoteInline,
+    ]
+
+class MembershipAdmin(admin.ModelAdmin):
+    inlines = [
+        NoteInline,
+    ]
+
 admin.site.register(Note)
-admin.site.register(MembershipTerm)
-admin.site.register(Organization)
-admin.site.register(Person)
-admin.site.register(Membership)
+admin.site.register(MembershipTerm, MembershipTermAdmin)
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Membership, MembershipAdmin)
