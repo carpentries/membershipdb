79e815b8b97837ab64e55f65a17bd7f1e7d2771e
Francisco Palm <francisco.palm@gmail.com>
2018-06-18 22:28:16 -0400

    Add django_extensions

--- membershipdb/settings.py
+++ membershipdb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'djmoney',
     'django_countries',
     'phonenumber_field',
