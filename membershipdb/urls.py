d8e415e07151de54672e070a019ca6d93816ec88
Francisco Palm <francisco.palm@gmail.com>
2018-07-06 11:25:16 -0400

    Modified url paths for views

--- membershipdb/urls.py
+++ membershipdb/urls.py
@@ -2,24 +2,18 @@ import re
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from accounts import views as accounts_views
+from membership import views as membership_views
 
 urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
-    url(r'', include('membership.urls')),
+    url(r'^$', membership_views.home, name='home'),
+    url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^organizations/$', membership_views.organization_list, name='organization_list'),
+    url(r'^memberships/$', membership_views.membership_list, name='membership_list'),
+    url(r'^contacts/$', membership_views.contact_list, name='contact_list'),
+    url(r'^terms/$', membership_views.term_list, name='term_list'),
+    url(r'^notes/$', membership_views.note_list, name='note_list'),
 ]
 
-# urlpatterns = [
-#     #path('', views.home, name='home'),
-    
-#     url(r'^$', 'membership.views.home', name='home'),
-# ]
-
-# urlpatterns = [
-#     url(r'^$', 'membership.views.home', name='home'),
-#     url(r'^admin/', include(admin.site.urls)),
-# ]
-
-# urlpatterns += [
-#     url(r'^membership/', include('membership.urls')),
-# ]
