6bc07e84af1bfa4749dcb830bbf2bf017c33b93d
Francisco Palm <francisco.palm@gmail.com>
2018-08-02 08:11:26 -0400

    Ahorten and beautify urls

--- membershipdb/urls.py
+++ membershipdb/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls import include, url
+"""MembershipDB project urls
+"""
+
+from django.conf.urls import include
 from django.urls import path
 from django.contrib import admin
 
@@ -12,21 +15,81 @@ urlpatterns = [
     path('signup/', accounts_views.signup, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
 
-    path('organizations/', membership_views.organization_list, name='organization_list'),
-    path('memberships/', membership_views.membership_list, name='membership_list'),
-    path('contacts/', membership_views.contact_list, name='contact_list'),
-    path('terms/', membership_views.term_list, name='term_list'),
-    path('notes/', membership_views.note_list, name='note_list'),
+    path(
+        'organizations/',
+        membership_views.organization_list,
+        name='organization_list'
+        ),
+    path(
+        'memberships/',
+        membership_views.membership_list,
+        name='membership_list'
+        ),
+    path(
+        'contacts/',
+        membership_views.contact_list,
+        name='contact_list'
+        ),
+    path(
+        'terms/',
+        membership_views.term_list,
+        name='term_list'
+        ),
+    path(
+        'notes/',
+        membership_views.note_list,
+        name='note_list'
+        ),
 
-    path('organization/<int:id>/', membership_views.organization_id, name='organization_id'),
-    path('membership/<int:id>/', membership_views.membership_id, name='membership_id'),
-    path('contact/<int:id>/', membership_views.contact_id, name='contact_id'),
-    path('term/<int:id>/', membership_views.term_id, name='term_id'),
-    path('note/<int:id>/', membership_views.note_id, name='note_id'),
+    path(
+        'organization/<int:org_id>/',
+        membership_views.organization_id,
+        name='organization_id'
+        ),
+    path(
+        'membership/<int:mmb_id>/',
+        membership_views.membership_id,
+        name='membership_id'
+        ),
+    path(
+        'contact/<int:ctc_id>/',
+        membership_views.contact_id,
+        name='contact_id'
+        ),
+    path(
+        'term/<int:trm_id>/',
+        membership_views.term_id,
+        name='term_id'
+        ),
+    path(
+        'note/<int:nt_id>/',
+        membership_views.note_id,
+        name='note_id'
+        ),
 
-    #path(r'^organization/(?P<id>\d+)/organization_form/$', membership_views.organization_form, name='organization_form'),
-    #path(r'^organization_edit/(?P<id>\d+)/$', membership_views.organization_edit, name='organization_edit'),
-    path('membership_edit/<int:id>/', membership_views.membership_edit, name='membership_edit'),
-    path('contact_edit/(?P<id>\d+)/', membership_views.contact_edit, name='contact_edit'),
+    path(
+        'organization_edit/<int:org_id>/',
+        membership_views.organization_edit,
+        name='organization_edit'
+        ),
+    path(
+        'membership_edit/<int:mmb_id>/',
+        membership_views.membership_edit,
+        name='membership_edit'
+        ),
+    path(
+        'contact_edit/<int:ctc_id>/',
+        membership_views.contact_edit,
+        name='contact_edit'
+        ),
+    path(
+        'term_edit/<int:trm_id>/',
+        membership_views.term_edit,
+        name='term_edit'
+        ),
+    path(
+        'note_edit/<int:nt_id>/',
+        membership_views.note_edit,
+        name='note_edit'
+        ),
 ]
-
